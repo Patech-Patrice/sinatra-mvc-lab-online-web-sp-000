29e6484bcf96daaa17b5ac9e882bb069c5e7e62f
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-21 18:12:31 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,7 +6,7 @@ class PigLatinizer
 
   def piglatinize(word)
     parts_of_word = word.split(/([^aeiouAEIOU]*)([aeiouAEIOU]*)(.*)/)
-    start = parts_of_word[1] 
+    start = parts_of_word[1]
     rest = parts_of_word[2] + (parts_of_word[3] || "")
     if start.length>0
       "#{rest}#{start}ay"
